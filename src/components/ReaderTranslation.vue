b244a79b9fd798ab65619dc6d32eac2b2a09fd92
yap8 <nebozhenko.arseny@gmail.com>
2022-05-28 16:24:27 +0300

    added whitespace nowrap and font semibold to translation  title

--- src/components/ReaderTranslation.vue
+++ src/components/ReaderTranslation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="absolute -top-2 -left-2 px-4 py-2 bg-white rounded shadow z-30">
-    <h4 class="mb-2">{{ word }}</h4>
+    <h4 class="whitespace-nowrap font-semibold mb-2">{{ word }}</h4>
     <div class="flex flex-col">
       <ReaderTranslationButton
         :class="{ 'mt-1': index }"
