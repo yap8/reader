d94614ccb98d08e8d4c0cbab2593eaf10d7ba7ec
yap8 <nebozhenko.arseny@gmail.com>
2022-05-28 17:25:48 +0300

    header: added export and changed hover style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,15 +4,18 @@
       <!-- <h1 class="text-2xl text-white">Document_name</h1> -->
       <nav class="flex justify-end w-full">
         <router-link
-          class="text-xl text-white p-2 mr-2 transition hover:text-gray-300"
+          class="text-xl text-white p-2 mr-2 transition hover:underline"
           to="/"
           >Read</router-link
         >
         <router-link
-          class="text-xl text-white p-2 transition hover:text-gray-300"
+          class="text-xl text-white p-2 mr-2 transition hover:underline"
           to="/import"
           >Import</router-link
         >
+        <button class="text-xl text-white p-2 transition hover:underline">
+          Export
+        </button>
       </nav>
     </div>
   </header>
