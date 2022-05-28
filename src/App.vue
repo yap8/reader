756c28ae2a87977682c8ea5de2daa3dd3acdebca
yap8 <nebozhenko.arseny@gmail.com>
2022-05-28 14:58:37 +0300

    added title and text

--- src/App.vue
+++ src/App.vue
@@ -12,3 +12,9 @@ export default {
   },
 };
 </script>
+
+<style>
+body {
+  @apply bg-gray-100;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="home"></div>
+  <div
+    class="container max-w-4xl text-xl bg-white mt-10 p-10 shadow rounded-lg whitespace-pre-line"
+  >
+    <h1 class="font-semibold text-3xl text-center mb-4">{{ title }}</h1>
+    <p>{{ text }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HomePage',
+  data() {
+    return {
+      title: `Je m’appelle Jessica`,
+      text: `Je m’appelle Jessica. Je suis une fille, je suis française et j’ai treize ans. Je vais à l’école à Nice, mais j’habite à Cagnes-Sur-Mer. J’ai deux frères. Le premier s’appelle Thomas, il a quatorze ans. Le second s’appelle Yann et il a neuf ans. Mon papa est italien et il est fleuriste. Ma mère est allemande et est avocate. Mes frères et moi parlons français, italien et allemand à la maison. Nous avons une grande maison avec un chien, un poisson et deux chats.
+
+Aujourd’hui, on est samedi, nous rendons visite à notre grand-mère. Elle a 84 ans et elle habite à Antibes. J’adore ma grand-mère, elle est très gentille. Elle fait des bons gâteaux.
+
+Lundi, je retourne à l’école. Je suis contente, je vais voir Amélie. C’est ma meilleure amie. J’aime beaucoup l’école. Mes matières préférées sont le français et le sport. J’aime beaucoup lire et je nage très bien.`,
+    };
+  },
 };
 </script>
