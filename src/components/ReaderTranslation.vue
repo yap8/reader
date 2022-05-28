3a60e867001b2b48350bcef0900c971b4758a942
yap8 <nebozhenko.arseny@gmail.com>
2022-05-28 16:22:36 +0300

    moved button into separate component

--- src/components/ReaderTranslation.vue
+++ src/components/ReaderTranslation.vue
@@ -1,28 +1,34 @@
 <template>
-  <div class="absolute -top-2 px-4 py-2 bg-white rounded shadow z-30">
-    <h4 class="mb-3">{{ word }}</h4>
+  <div class="absolute -top-2 -left-2 px-4 py-2 bg-white rounded shadow z-30">
+    <h4 class="mb-2">{{ word }}</h4>
     <div class="flex flex-col">
-      <button
-        class="whitespace-nowrap py-1 px-4 mb-1 text-base text-white bg-blue-500 rounded transition hover:bg-blue-600"
+      <ReaderTranslationButton
+        :class="{ 'mt-1': index }"
+        v-for="(translation, index) in translations"
+        :key="translation + index"
+        >{{ translation }}</ReaderTranslationButton
       >
-        Translation 1
-      </button>
-      <button
-        class="whitespace-nowrap py-1 px-4 text-base text-white bg-blue-500 rounded transition hover:bg-blue-600"
-      >
-        Translation 1
-      </button>
     </div>
   </div>
 </template>
 
 <script>
+import ReaderTranslationButton from './ReaderTranslationButton';
+
 export default {
+  components: {
+    ReaderTranslationButton,
+  },
   props: {
     word: {
       type: String,
       required: true,
     },
+  },
+  data() {
+    return {
+      translations: ['Translation 1', 'Translation 2', 'Translation 3'],
+    };
   },
 };
 </script>
---
+++ src/components/ReaderTranslationButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="whitespace-nowrap py-1 px-4 text-base text-white bg-blue-500 rounded transition hover:bg-blue-600"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
