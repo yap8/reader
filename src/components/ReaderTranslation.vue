bbe8692ab4d3619c4af8f0ba7cd4cac425b39365
yap8 <nebozhenko.arseny@gmail.com>
2022-05-29 19:42:05 +0300

    ".", ",", ";" are now removed in reader translation

--- src/components/ReaderTranslation.vue
+++ src/components/ReaderTranslation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="absolute -top-2 -left-2 px-4 py-2 bg-white rounded shadow z-30">
-    <h4 class="whitespace-nowrap font-semibold mb-2">{{ word }}</h4>
+    <h4 class="whitespace-nowrap font-semibold mb-2">{{ filteredWord }}</h4>
     <div class="flex flex-col" v-if="translations">
       <ReaderTranslationButton
         :class="{ 'mt-1': index }"
@@ -33,7 +33,10 @@ export default {
       translations: null,
     };
   },
+  computed: {
+    filteredWord() {
+      return this.word.replace(/\.|,|;/g, '');
+    },
+  },
 };
 </script>
-
-<style></style>
