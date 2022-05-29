77ce3ff0ed8a24d771c903d9ba21d09b190dabcc
yap8 <nebozhenko.arseny@gmail.com>
2022-05-29 19:48:18 +0300

    made translation button smaller

--- src/components/ReaderTranslationButton.vue
+++ src/components/ReaderTranslationButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="whitespace-nowrap py-1 px-4 text-base text-white bg-blue-500 rounded transition hover:bg-blue-600"
+    class="whitespace-nowrap py-1 px-2 text-base text-white bg-blue-500 rounded transition hover:bg-blue-600"
   >
     <slot />
   </button>
